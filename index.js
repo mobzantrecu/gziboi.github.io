5bee5988c707e39d7a8223bccba1dda1e09759c9
mobzantrecu <nicolas.rodriguezlb@gmail.com>
2020-10-03 18:42:49 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function circularText(txt, radius, classIndex) {
+    txt = txt.split(""),
+        classIndex = document.getElementsByClassName("circTxt")[classIndex];
+
+    var deg = 360 / txt.length,
+        origin = 0;
+
+    txt.forEach((ea) => {
+        ea = `<p style='height:${radius}px;position:absolute;transform:rotate(${origin}deg);transform-origin:0 100%'>${ea}</p>`;
+        classIndex.innerHTML += ea;
+        origin += deg;
+    });
+    }
